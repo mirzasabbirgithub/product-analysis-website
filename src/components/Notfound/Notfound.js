6bf49be90042337f881362b55d4c87d849f62658
mirzasabbirgithub <mirzaasabbir@gmail.com>
2022-04-04 20:18:52 +0600

    Notfound page updated

--- src/components/Notfound/Notfound.js
+++ src/components/Notfound/Notfound.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import '../Notfound/Notfound.css';
 const Notfound = () => {
           return (
                     <div>
-                              <h2>Page not found</h2>
-                              <h3>404</h3>
+                              <h1>Page Not Found</h1>
+                              <img src="https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-404.png" alt="error-pictures" />
                     </div>
           );
 };
