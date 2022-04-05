fe0b82720c779ff46bebb899cbd634186586a623
mirzasabbirgithub <mirzaasabbir@gmail.com>
2022-04-05 11:48:31 +0600

    404 error responsiveness fixed

--- src/components/Notfound/Notfound.js
+++ src/components/Notfound/Notfound.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import '../Notfound/Notfound.css';
 const Notfound = () => {
           return (
-                    <div>
-                              <h1>Page Not Found</h1>
-                              <img src="https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-404.png" alt="error-pictures" />
-                    </div>
+                    <Container>
+                              <Row>
+                                        <Col className='img-fluid'>
+                                                  <h1>Page Not Found</h1>
+                                                  <img src="https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-404.png" alt="error-pictures" />
+                                        </Col>
+                              </Row>
+                    </Container>
           );
 };
 
